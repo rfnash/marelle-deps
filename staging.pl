ba755fbd05078c851b89157b39e99236fc508864
Robert F. Nash <robert@robertnash.net>
2014-05-19 01:18:18 -0400

    staging: currently installed, but tempory packages

---
+++ staging.pl
@@ -0,0 +1,12 @@
+% Needed to acess box.net
+% TODO: Probably has the same name under Archlinux
+debian_pkg('davfs2').
+
+% Needed by atom
+debian_pkg('libgnome-keyring-dev').
+
+% Needed by vimb
+debian_pkg('libwebkitgtk-3.0-dev').
+
+% TODO: My own package that must be installed manually.
+debian_pkg('vimb-git').
