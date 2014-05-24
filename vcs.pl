c6b0d504bcbf5c3c3e8d7763e0ea56d5cb980391
Robert F. Nash <robert@robertnash.net>
2014-05-24 11:01:05 -0400

    consolidate related packages as a single package

--- vcs.pl
+++ vcs.pl
@@ -2,15 +2,12 @@
 % vcs.pl
 % marelle-deps
 %
-debian_pkg('aegis-doc').
-debian_pkg('aegis-tk').
+debian_pkg('ageis', ['aegis-tk', 'aegis-doc']).
 linux_pkg('bzr', pacman('bzr'), 'bzr').
 linux_pkg('cvs', pacman('cvs'), 'cvs').
 linux_pkg('dvcs-autosync', aur('dvcs-autosync-git'), 'dvcs-autosync').
 linux_pkg('fossil', pacman('fossil'), 'fossil').
-debian_pkg('git-doc').
-debian_pkg('git-gui').
-linux_pkg('git', pacman('git'), 'git').
+linux_pkg('git', pacman('git'), ['git', 'git-doc', 'git-gui']).
 linux_pkg('mercurial', pacman('mercurial'), 'mercurial').
 linux_pkg('subversion', pacman('subversion'), 'subversion').
 linux_pkg('tig', pacman('tig'), 'tig').
