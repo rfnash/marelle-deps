2371cf2bfd658bf3ffdcb10f93905e0d8cec73b1
Robert F. Nash <robert@robertnash.net>
2014-05-24 16:09:55 -0400

    office.pl: removed unnecesary packages

--- office.pl
+++ office.pl
@@ -2,71 +2,37 @@
 % office.pl
 % marelle-deps
 %
-linux_pkg('asciidoc', pacman('asciidoc'), 'asciidoc').
 linux_pkg('aspell-en', pacman('aspell-en'), 'aspell-en').
 linux_pkg('calibre', pacman('calibre'), 'calibre').
-debian_pkg('ceni').
-linux_pkg('cuneiform', pacman('cuneiform'), 'cuneiform').
 debian_pkg('dar').
-linux_pkg('devede', pacman('devede'), 'devede').
-debian_pkg('devtodo').
-debian_pkg('docbook-xml').
 linux_pkg('dropbox', pacman('dropbox'), 'nautilus-dropbox').
 linux_pkg('dtrx', aur('dtrx'), 'dtrx').
-debian_pkg('dvdstyler').
-linux_pkg('freemind', pacman('freemind'), 'freemind').
-linux_pkg('freeplane', aur('freeplane'), 'freeplane').
 linux_pkg('gnucash', pacman('gnucash'), 'gnucash').
 linux_pkg('gnumeric', pacman('gnumeric'), 'gnumeric').
 debian_pkg('gnupg-agent').
 aur_pkg('gnupg1').
-linux_pkg('gocr', pacman('gocr'), 'gocr').
-linux_pkg('gpa', aur('gpa'), 'gpa').
-aur_pkg('gpwsafe').
 linux_pkg('hunspell-en', pacman('hunspell-en'), 'hunspell-en-us').
-linux_pkg('josm', pacman('josm'), 'josm').
-linux_pkg('keepassx', pacman('keepassx'), 'keepassx').
 linux_pkg('keychain', pacman('keychain'), 'keychain').
 debian_pkg('latexmk').
 linux_pkg('ledger', aur('ledger'), 'ledger').
-linux_pkg('markdown', pacman('markdown'), 'markdown').
-debian_pkg('mupdf').
-debian_pkg('mupdf-tools').
 debian_pkg('nautilus-dropbox').
-aur_pkg('nested').
-aur_pkg('nixnote').
-linux_pkg('notmuch', pacman('notmuch'), 'notmuch').
 linux_pkg('p7zip', pacman('p7zip'), 'p7zip').
 debian_pkg('pandoc').
 linux_pkg('paperkey', aur('paperkey'), 'paperkey').
 linux_pkg('parcimonie', aur('parcimonie-sh-git'), 'parcimonie').
-linux_pkg('password-gorilla', aur('password-gorilla'), 'password-gorilla').
-debian_pkg('passwordsafe').
-aur_pkg('passwordsafe-debian').
-linux_pkg('pcmanfm', pacman('pcmanfm'), 'pcmanfm').
-aur_pkg('punch-time-tracking').
-debian_pkg('ranger').
-debian_pkg('remembrance-agent').
+linux_pkg('passwordsafe', aur('passwordsafe-debian'), 'passwordsafe').
 linux_pkg('sharutils', pacman('sharutils'), 'sharutils').
 linux_pkg('slrn', pacman('slrn'), 'slrn').
-linux_pkg('sparkleshare', pacman('sparkleshare'), 'sparkleshare').
-linux_pkg('tasque', pacman('tasque'), 'tasque').
-linux_pkg('tesseract', pacman('tesseract'), 'tesseract-ocr').
-debian_pkg('tesseract-ocr').
-aur_pkg('todotxt').
 linux_pkg('unar', pacman('unar'), 'unar').
 linux_pkg('units', pacman('units'), 'units').
 linux_pkg('unzip', pacman('unzip'), 'unzip').
-aur_pkg('vue').
 linux_pkg('words', pacman('words'), 'wamerican').
-debian_pkg('xarchiver').
 debian_pkg('xdelta3').
 linux_pkg('xmind', pacman('xmind'), 'xmind').
 debian_pkg('xsane').
 pacman_pkg('xsane-gimp').
-linux_pkg('yagf', pacman('yagf'), 'yagf').
 debian_pkg('zathura').
-linux_pkg('zathura-djvu', pacman('zathura-djvu'), 'zathura-djvu').
-pacman_pkg('zathura-pdf-mupdf').
-linux_pkg('zathura-ps', pacman('zathura-ps'), 'zathura-ps').
+linux_pkg('zathura-djvu',
+    pacman(['zathura-djvu','zathura-pdf-mupdf','zathura-ps']),
+    ['zathura-djvu','zathura-ps']).
 debian_pkg('zip').
