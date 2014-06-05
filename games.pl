0a82ac5f45f1f09efa22286a6077198a95807e84
Robert F. Nash <robert@robertnash.net>
2014-06-05 13:03:34 -0400

    games.pl: rearranged

--- games.pl
+++ games.pl
@@ -21,7 +21,10 @@ meta_pkg('games', [
     'tome',
     'wesnoth',
     %'xbattle',
-    'xmahjongg'
+    'xmahjongg',
+    'dwarffortress-ironhand',
+    'empire',
+    'xmille'
     ], linux(arch)).
 
 meta_pkg('games', [
@@ -40,66 +43,32 @@ meta_pkg('games', [
     'tome',
     'wesnoth',
     'xbattle',
-    'xmahjongg'
+    'xmahjongg',
+    'empire',
+    'xmille'
     ], linux(sid)).
 
 linux_pkg('angband', pacman('angband'), 'angband').
-
-
 linux_pkg('boswars', aur('boswars'), 'boswars').
-
-
 pacman_pkg('ccgo').
-
 linux_pkg('cgoban', pacman('cgoban'), 'cgoban').
-
-
 debian_pkg('crossfire-client').
 %aur_pkg('crossfire-client').
-
-
 linux_pkg('freedink', aur('freedink'), 'freedink').
-
-
 linux_pkg('gnubg', pacman('gnubg'), 'gnubg').
-
-
 linux_pkg('hyperrogue', aur('hyperrogue'), 'hyperrogue').
-
-
 debian_pkg('nethack-el').
 %aur_pkg('nethack-el').
-
-
 linux_pkg('nethack-x11', aur('nethack-x11'), 'nethack-x11').
-
-
 linux_pkg('oolite', pacman('oolite'), 'oolite').
-
-
 linux_pkg('stone-soup', pacman('stone-soup'), 'crawl-tiles').
-
-
 aur_pkg('terasology').
-
-
 linux_pkg('tome', aur('tome2'), 'tome').
-
-
 linux_pkg('wesnoth', pacman('wesnoth'), 'wesnoth').
-
-
 debian_pkg('xbattle').
 %pacman_pkg('xbattle').
-
-
 linux_pkg('xmahjongg', pacman('xmahjongg'), 'xmahjongg').
-
-
 linux_pkg('pokerth', pacman('pokerth'), 'pokerth').
-
-% TODO: add to metapackage
 linux_pkg('empire', aur('vms-empire'), 'empire').
 linux_pkg('xmille', aur('xmille'), 'xmille').
 aur_pkg('dwarffortress-ironhand').
-
