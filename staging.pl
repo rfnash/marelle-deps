d26014c8652b88988b4e1d680a1712f72c99f138
Robert F. Nash <robert@robertnash.net>
2014-06-05 13:16:16 -0400

    added various debian packages to staging

--- staging.pl
+++ staging.pl
@@ -10,3 +10,27 @@ debian_pkg('libwebkitgtk-3.0-dev').
 
 % TODO: My own package that must be installed manually.
 debian_pkg('vimb-git').
+debian_pkg('artha').
+debian_pkg('dico').
+debian_pkg('dico-doc').
+debian_pkg('dico-module-wordnet').
+debian_pkg('dict').
+debian_pkg('dict-foldoc').
+debian_pkg('dict-gcide').
+debian_pkg('dict-jargon').
+debian_pkg('dict-wn').
+debian_pkg('ffmpeg').
+debian_pkg('goldendict-wordnet').
+debian_pkg('gstreamer0.10-plugins-base-apps').
+debian_pkg('gstreamer1.0-plugins-bad').
+debian_pkg('gstreamer1.0-plugins-base-apps').
+debian_pkg('gstreamer1.0-plugins-ugly').
+debian_pkg('hexchat').
+debian_pkg('libnotify-bin').
+debian_pkg('miscfiles').
+debian_pkg('python-notify2').
+debian_pkg('python3-matplotlib').
+debian_pkg('samba-common').
+debian_pkg('stardict-common').
+debian_pkg('unrar').
+debian_pkg('unrar-free').
